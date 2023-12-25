96ca7d871a254e64c92c4e13883c06f912c0ecf3
EdCarlos-dev <bio_edc@hotmail.com>
2023-12-25 19:19:03 -0300

    first day files

---
+++ solutions/day-01/variables.js
@@ -0,0 +1,6 @@
+var Name = 'Asabeneh';
+Name = 'Teste';
+var PI = 3.14; // Não é permitido reatribuir PI a um novo valor
+// PI = 3.
+console.log(Name); // Imprime o valor de firstName
+console.log(PI); // Imprime o valor de PI
---
+++ solutions/day-01/variables.ts
@@ -0,0 +1,8 @@
+let Name: string = 'Asabeneh';
+Name = 'Teste';
+
+const PI: number = 3.14; // Não é permitido reatribuir PI a um novo valor
+// PI = 3.
+
+console.log(Name); // Imprime o valor de firstName
+console.log(PI); // Imprime o valor de PI
