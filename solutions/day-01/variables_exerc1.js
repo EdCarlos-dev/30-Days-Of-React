1d13508601d885f746793d8e1d3777b20d376ff1
EdCarlos-dev <bio_edc@hotmail.com>
2023-12-25 20:01:52 -0300

    valiables test

---
+++ solutions/day-01/variables_exerc1.js
@@ -0,0 +1,6 @@
+var firstNameName = 'Asabeneh';
+firstName = 'Eyob';
+var Pi = 3.14; // Não é permitido reatribuir PI a um novo valor
+// PI = 3.
+console.log(firstName); // Imprime o valor de firstName
+console.log(Pi); // Imprime o valor de PI
