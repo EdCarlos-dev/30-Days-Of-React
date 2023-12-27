d9e307c6ef91ca81af1ce050d4167d2673e775f8
EdCarlos-dev <bio_edc@hotmail.com>
2023-12-27 12:55:43 -0300

    update 03 day app

--- apps/day-03/src/index.js
+++ apps/day-03/src/index.js
@@ -1,50 +1,92 @@
-//index.js
 // index.js
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const headerStyles = {
-  backgroundColor: '#61DBFB',
-  fontFamily: 'Helvetica Neue',
-  padding: 25,
-  lineHeight: 1.5,
+// To get the root element from the HTML document
+import reactLogo from './images/logo192.png'
+// JSX element, header
+const welcome = 'Welcome to 30 Days Of React'
+const title = 'Getting Started React'
+const subtitle = 'JavaScript Library'
+const author = {
+  firstName: 'Ed Carlos',
+  lastName: 'Santos',
 }
+const date = 'Oct 2, 2020'
 
 // JSX element, header
 const header = (
-  <header style={headerStyles}>
+  <header>
     <div className='header-wrapper'>
-      <h1>App One</h1>
-      <h2>Getting Started React</h2>
-      <h3>JavaScript Library</h3>
-      <p>Asabeneh Yetayeh</p>
-      <small>Oct 2, 2020</small>
+      <h1>{welcome}</h1>
+      <h2>{title}</h2>
+      <h3>{subtitle}</h3>
+      <p>
+        Instructor: {author.firstName} {author.lastName}
+      </p>
+      <small>Date: {date}</small>
     </div>
   </header>
 )
 
+// const numOne = 3
+// const numTwo = 2
+
+// const result = (
+//   <p>
+//     {numOne} + {numTwo} = {numOne + numTwo}
+//   </p>
+// )
+
+const yearBorn = 1987
+const currentYear = new Date().getFullYear()
+const age = currentYear - yearBorn
+const personAge = (
+  <p>
+    {' '}
+    {author.firstName} {author.lastName} is {age} years old
+  </p>
+)
+
 // JSX element, main
-const mainStyles = {
-  backgroundColor: '#F3F0F5',
-}
+const techs = ['HTML', 'CSS', 'JavaScript' , 'Typescript']
+const techsFormatted = techs.map((tech) => <li>{tech}</li>)
+
+const user = (
+  <div>
+    <img src={reactLogo} alt='ReactLogo' />
+    <img src={reactLogo} alt='ReactLogo' />
+    <img src={reactLogo} alt='ReactLogo' />
+    <img src={reactLogo} alt='ReactLogo' />
+  </div>
+)
+
+// JSX element, main
 const main = (
-  <main style={mainStyles}>
-    <p>Prerequisite to get started react.js:</p>
-    <ul>
-      <li>HTML</li>
-      <li>CSS</li>
-      <li>JavaScript</li>
-    </ul>
+  <main>
+    <div className='main-wrapper'>
+      <p>
+        Prerequisite to get started{' '}
+        <strong>
+          <em>react.js</em>
+        </strong>
+        :
+      </p>
+      <ul>{techsFormatted}</ul>
+      {/* {result} */}
+      {personAge}
+      {user}
+    </div>
   </main>
 )
 
-const footerStyles = {
-  backgroundColor: '#61DBFB',
-}
+const copyRight = 'Copyright 2020'
+
 // JSX element, footer
 const footer = (
-  <footer style={footerStyles}>
-    <p>Copyright 2020</p>
+  <footer>
+    <div className='footer-wrapper'>
+      <p>{copyRight}</p>
+    </div>
   </footer>
 )
 
@@ -60,20 +102,3 @@ const app = (
 const rootElement = document.getElementById('root')
 // we render the JSX element using the ReactDOM package
 ReactDOM.render(app, rootElement)
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
