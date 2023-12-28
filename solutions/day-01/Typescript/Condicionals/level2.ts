027d798c6c47cd1e996bb69cf3ec14f5d1d9e6ef
EdCarlos-dev <bio_edc@hotmail.com>
2023-12-27 22:28:58 -0300

    update condicional and node modules

--- solutions/day-01/Typescript/Condicionals/level2.ts
+++ solutions/day-01/Typescript/Condicionals/level2.ts
@@ -6,11 +6,69 @@
 // 50-59, D
 // 0-49, F
 
+function assignGrade(score: number): string {
+    if (score >= 80 && score <= 100) {
+      return 'A';
+    } else if (score >= 70 && score <= 79) {
+      return 'B';
+    } else if (score >= 60 && score <= 69) {
+      return 'C';
+    } else if (score >= 50 && score <= 59) {
+      return 'D';
+    } else if (score >= 0 && score <= 49) {
+      return 'F';
+    } else {
+      return 'Invalid score';
+    }
+  }
+  
+  // Solicita a pontuação do aluno através do console
+  let userInput: string | null = prompt('Enter student score:');
+  
+  if (userInput !== null) {
+    const score: number = parseInt(userInput);
+    const grade: string = assignGrade(score);
+    console.log(`Student grade is: ${grade}`);
+  }
+  
+
 // 2 Check if the season is Autumn, Winter, Spring or Summer. If the user input is :
 // September, October or November, the season is Autumn.
 // December, January or February, the season is Winter.
 // March, April or May, the season is Spring
 // June, July or August, the season is Summer
+
+// Solicita ao usuário que insira o mês
+let userInputMonth: string | null = prompt('Enter a month:');
+
+if (userInputMonth !== null) {
+  const month: string = userInputMonth.toLowerCase();
+
+  switch (month) {
+    case 'september':
+    case 'october':
+    case 'november':
+      console.log('The season is Autumn.');
+      break;
+    case 'december':
+    case 'january':
+    case 'february':
+      console.log('The season is Winter.');
+      break;
+    case 'march':
+    case 'april':
+    case 'may':
+      console.log('The season is Spring.');
+      break;
+    case 'june':
+    case 'july':
+    case 'august':
+      console.log('The season is Summer.');
+      break;
+    default:
+      console.log('Invalid month input.');
+  }
+}
 
 // 3 Check if a day is weekend day or a working day. Your script will take day as an input.
 
@@ -25,3 +83,26 @@
 
 // What is the day today? FrIDAy
 // Friday is a working day.
+
+// Solicita ao usuário que insira o dia
+let userInputDay: string | null = prompt('What is the day today?');
+
+if (userInputDay !== null) {
+  const day: string = userInputDay.toLowerCase();
+
+  switch (day) {
+    case 'saturday':
+    case 'sunday':
+      console.log(`${day} is a weekend.`);
+      break;
+    case 'monday':
+    case 'tuesday':
+    case 'wednesday':
+    case 'thursday':
+    case 'friday':
+      console.log(`${day} is a working day.`);
+      break;
+    default:
+      console.log('Invalid day input.');
+  }
+}
--- solutions/day-01/Typescript/Condicionals/level3.ts
+++ solutions/day-01/Typescript/Condicionals/level3.ts
@@ -13,4 +13,77 @@
 // Enter a month: FEbruary
 // February has 28 days.
 
+// Solicitar ao usuário que insira o mês
+let userInput: string | null = prompt('Enter a month:');
+
+if (userInput !== null) {
+  const month: string = userInput.toLowerCase();
+
+  switch (month) {
+    case 'january':
+    case 'march':
+    case 'may':
+    case 'july':
+    case 'august':
+    case 'october':
+    case 'december':
+      console.log(`${month} has 31 days.`);
+      break;
+    case 'april':
+    case 'june':
+    case 'september':
+    case 'november':
+      console.log(`${month} has 30 days.`);
+      break;
+    case 'february':
+      console.log(`${month} has 28 days.`);
+      break;
+    default:
+      console.log('Invalid month input.');
+  }
+}
+
 // 1 Write a program which tells the number of days in a month, now consider leap year.
+
+// Solicitar ao usuário que insira o mês e o ano
+let userInputMonth: string | null = prompt('Enter a month:');
+let userInputYear: string | null = prompt('Enter a year:');
+
+if (userInputMonth !== null && userInputYear !== null) {
+  const month: string = userInputMonth.toLowerCase();
+  const year: number = parseInt(userInputYear);
+
+  let daysInMonth: number;
+
+  switch (month) {
+    case 'january':
+    case 'march':
+    case 'may':
+    case 'july':
+    case 'august':
+    case 'october':
+    case 'december':
+      daysInMonth = 31;
+      break;
+    case 'april':
+    case 'june':
+    case 'september':
+    case 'november':
+      daysInMonth = 30;
+      break;
+    case 'february':
+      if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+        daysInMonth = 29;
+      } else {
+        daysInMonth = 28;
+      }
+      break;
+    default:
+      console.log('Invalid month input.');
+      daysInMonth = -1;
+  }
+
+  if (daysInMonth !== -1) {
+    console.log(`${month} in year ${year} has ${daysInMonth} days.`);
+  }
+}
